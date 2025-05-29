4c936e795de684522d3cfc484eb2696aa41028b1
Prateek Mishra <pm921670@gmail.com>
2025-05-29 11:31:58 +0530

    fix

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+import HomePage from "./Pages/HomePage.jsx"
+import About from "./Pages/About.jsx"
+import Price from "./Pages/Price.jsx"
+import Projects from "./Pages/Projects.jsx"
+import Contact from "./Pages/Contact.jsx"
+const App = () => {
+  return (
+ <>
+  <Router>
+    <Routes>
+      <Route path='/' element={<HomePage/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/projects' element={<Projects/>}/>
+      <Route path='/price' element={<Price/>}/>
+      <Route path='/contact-us' element={<Contact/>}/>
+    </Routes>
+  </Router>
+ </>
+  )
+}
+
+export default App
